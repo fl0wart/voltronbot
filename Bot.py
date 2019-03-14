5464f821f6dd6bb33c4f3c76b40556b6ac91d1d6
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-14 11:06:34 +0200

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,53 @@
+himport discord
+from discord.ext import commands
+import asyncio
+import youtube_dl
+import time
+import os
+
+
+bot=commands.Bot(command_prefix=';')
+@bot.event
+async def on_ready():
+    print('Logged in as '+bot.user.name+' (ID:'+bot.user.id+') | Connected to '+str(len(bot.servers))+' servers | Connected to '+str(len(set(bot.get_all_members())))+' users')
+    print('--------')
+    print('--------')
+    print('Bot is now online ')
+    print('Created by fl0w.')
+    bot.loop.create_task(status_task())    	 	 	 	 	 
+@bot.command(pass_context=True)
+async def ping(ctx):
+    t = await bot.say('Pong!')
+    ms = (t.timestamp-ctx.message.timestamp).total_seconds() * 1000
+    await bot.edit_message(t, new_content='Pong! Took: {}ms'.format(int(ms)))
+               
+@bot.command(pass_context = True)
+@commands.has_permissions(manage_messages = True)
+async def purge(ctx, number: int):
+  purge = await bot.purge_from(ctx.message.channel, limit = number+1)
+bot.say('Succesfully purged the messages!')
+
+@bot.command(pass_context=True)
+@commands.cooldown(1, 60*60*24, commands.BucketType.user)
+async def daily(ctx):
+    t = await bot.say('Pong!')
+    ms = (t.timestamp-ctx.message.timestamp).total_seconds() * 30
+    await bot.edit_message(t, new_content=':white_check_mark: You  received:  {}daily coins'.format(int(ms)))
+bot.say('Cooldown is 24 hours!')                                                                     			                           
+@bot.event	                                                
+async def status_task():
+    while True:
+        await bot.change_presence(game=discord.Game(name=';help', type=2))
+        await asyncio.sleep(5)
+        await bot.change_presence(game=discord.Game(name=str(len(set(bot.get_all_members())))+' users', type=3))
+        await asyncio.sleep(5)
+        await bot.change_presence(game=discord.Game(name=str(len(bot.servers))+' servers', type=3))
+        await asyncio.sleep(5)
+        await bot.change_presence(game=discord.Game(name='Made by fl0w.'))
+        await asyncio.sleep(5)
+        await bot.change_presence(game=discord.Game(name='the server', type=3))
+        await asyncio.sleep(5)
+        await bot.change_presence(game=discord.Game(name='fl0w.', type=2))
+        await asyncio.sleep(5)	
+        
+bot.run('NTU0MzczODAwMDAzNzY0MjQ0.D2bt9w.GYUh2zWLCKCoj8eVYN76E9aUhfk')
